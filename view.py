2b48bf77f3e9d7884d0646b459efc14063980afa
chrisbillows <christopherbillows@gmail.com>
2023-05-15 12:52:17 +0100

    Add basic buttons

--- view.py
+++ view.py
@@ -4,6 +4,14 @@ from tkinter import ttk
 class View(tk.Tk):
     
     PAD = 10
+    MAX_BUTTONS_PER_ROW = 4
+    button_captions = [
+        'C', '+/-', '%', '/',
+        7, 8, 9, '*',
+        4, 5, 6, '-',
+        1, 2, 3, '+',
+        0, '.', '='
+    ]
     
     def __init__(self, controller) -> None:
         super().__init__()  # initialise superclass e.g. an instance of Tk class
@@ -12,6 +20,7 @@ class View(tk.Tk):
         self.value_var = tk.StringVar()
         self._make_main_frame()
         self._make_entry()
+        self._make_buttons()
         
     def main(self):
         self.mainloop()
@@ -22,8 +31,16 @@ class View(tk.Tk):
                     
         
     def _make_entry(self):
-        ent = ttk.Entry(self.main_frm, textvariable=self.value_var)
+        ent = ttk.Entry(self.main_frm, justify='right', textvariable=self.value_var)
         ent.pack()
+        
+    def _make_buttons(self):
+        frm = ttk.Frame(self.main_frm)
+        frm.pack ()
+        
+        for caption in self.button_captions:
+            btn = ttk.Button(frm, text=caption)
+            btn.pack()
         
         
         
