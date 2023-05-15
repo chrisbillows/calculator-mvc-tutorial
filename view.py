cc2885e753163da2dc99ce2c9d526c5fc35a87c1
chrisbillows <christopherbillows@gmail.com>
2023-05-15 12:42:05 +0100

    Create initial tkinter window & entry

--- view.py
+++ view.py
@@ -1,8 +1,29 @@
-class View:
+import tkinter as tk
+from tkinter import ttk
+
+class View(tk.Tk):
+    
+    PAD = 10
     
     def __init__(self, controller) -> None:
-        self.controler = controller
+        super().__init__()  # initialise superclass e.g. an instance of Tk class
+        self.title("PyCalc 1.0")
+        self.controller = controller
+        self.value_var = tk.StringVar()
+        self._make_main_frame()
+        self._make_entry()
         
     def main(self):
-        print("In main of view")
+        self.mainloop()
+        
+    def _make_main_frame(self):
+        self.main_frm = ttk.Frame(self)
+        self.main_frm.pack(padx=self.PAD, pady=self.PAD)
+                    
+        
+    def _make_entry(self):
+        ent = ttk.Entry(self.main_frm, textvariable=self.value_var)
+        ent.pack()
+        
+        
         
