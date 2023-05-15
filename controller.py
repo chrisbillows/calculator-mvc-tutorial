5cab1db44c08d49c69b4401713e0abf11b88464f
chrisbillows <christopherbillows@gmail.com>
2023-05-15 16:02:55 +0100

    Complete full functionality

--- controller.py
+++ controller.py
@@ -11,7 +11,8 @@ class Controller:
         self.view.main()
         
     def on_button_click(self, caption):
-        print(f"button {caption} clicked")
+        result = self.model.calculate(caption)
+        self.view.value_var.set(result)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,2 +1,47 @@
 class Model:
-    pass
+    
+    def __init__(self) -> None:
+        self.previous_value = ''
+        self.value = ''
+        self.operator = ''
+    
+    def calculate(self, caption):
+        if caption == 'C':
+            self.value = ''
+            self.previous_value = ''
+            self.operator = ''
+        elif isinstance(caption, int):
+            self.value += str(caption)
+            
+        elif caption == '+/-':
+            self.value = int(self.value) * -1    
+            # self.value = self.value[1:] if self.value[0] == '-' else '-' + self.value
+        
+        elif caption == '%':
+            value = float(self.value) if '.' in self.value else int(self.value)
+            
+            self.value = str(value / 100) 
+        
+        elif caption == '.':
+            if not caption in self.value:
+                self.value += caption 
+        
+        elif caption == '=':
+            value = self._evaluate()
+            
+            if '.0' in str(value):
+                value = int(value)
+            
+            self.value = str(value)
+        
+        
+        else:
+            if self.value:
+                self.operator = caption    
+                self.previous_value = self.value
+                self.value = ''       
+            
+        return self.value
+    
+    def _evaluate(self):
+        return eval(self.previous_value + self.operator + self.value)
